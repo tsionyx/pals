ac331900a53cca622b6fd1b7f34979dbe1076ff4
Ivan Ladelshchikov <tsionyx@gmail.com>
2020-10-22 12:12:15 +0500

    complete the first

--- src/bin/ch1_1.rs
+++ src/bin/ch1_1.rs
@@ -1,2 +1,13 @@
+use base64::encode;
+use pals::parse_hex;
+
+const HEX_REPR: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
 fn main() {
+    eprintln!("Hex representation: {}", HEX_REPR);
+
+    let raw = parse_hex(HEX_REPR);
+    eprintln!("Raw bytes representation: {:?}", &raw);
+
+    println!("{}", encode(&raw));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
 #![allow(clippy::must_use_candidate)]
+
+use itertools::Itertools;
+
+/// <https://codereview.stackexchange.com/a/201699>
+pub fn parse_hex(hex_asm: &str) -> Vec<u8> {
+    let hex_bytes = hex_asm
+        .as_bytes()
+        .iter()
+        .filter_map(|b| match b {
+            b'0'..=b'9' => Some(b - b'0'),
+            b'a'..=b'f' => Some(b - b'a' + 10),
+            b'A'..=b'F' => Some(b - b'A' + 10),
+            _ => None,
+        })
+        .fuse();
+
+    hex_bytes.tuples().map(|(h, l)| h << 4 | l).collect()
+}
